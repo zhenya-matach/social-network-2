7e90851374dc65719097a3236d2bc0656239b2d8
Yauheni Matach <zhenya.matach@gmail.com>
2025-05-12 01:25:48 +0300

    Commit-1

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <header className="header">
+            <img src="https://logowik.com/content/uploads/images/purple-social-media8072.jpg" alt=""/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div><img
+                src="https://img.freepik.com/free-vector/low-poly-style-abstract-backdrop-modern-presentation_1017-53594.jpg?semt=ais_hybrid&w=740"
+                alt=""/></div>
+            <div>ava+description</div>
+            <div>My posts
+                <div>New post</div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
