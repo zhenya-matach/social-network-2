27900ad235db2c9c0a6c542d797de282c6eb95f8
Yauheni Matach <zhenya.matach@gmail.com>
2025-05-20 14:10:48 +0300

    21. Dialogue Page Layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,21 @@
 import style from './Dialogs.module.css';
 
-const Dialogs = (props) => {
+const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog}>Dimych</div>
+                <div className={style.dialog}>Andrey</div>
+                <div className={style.dialog}>Svetlana</div>
+                <div className={style.dialog}>Yauheni</div>
+                <div className={style.dialog + " " + style.active}>Julia</div>
+                <div className={style.dialog}>Valera</div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How are your it-kamasutra?</div>
+                <div className={style.message}>Yo, bro!</div>
+            </div>
         </div>
     )
 }
