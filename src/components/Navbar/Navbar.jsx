d5ee887e251eb15226b657fec45235e8b9792061
Yauheni Matach <zhenya.matach@gmail.com>
2025-05-20 12:26:38 +0300

    19.1 Updating dependencies

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,13 @@
-import './Navbar.module.css';
 import style from './Navbar.module.css';
 
 const Navbar = () => {
     return (
         <nav className={style.nav}>
-            <div className={style.item}>Profile</div>
-            <div className={`${style.item} ${style.active}`}>Messages</div>
-            <div className={style.item}>News</div>
-            <div className={style.item}>Music</div>
-            <div className={style.item}>Settings</div>
+            <div className={style.item}><a href='/Profile'>Profile</a></div>
+            <div className={`${style.item} ${style.active}`}><a href='/Messages'>Messages</a></div>
+            <div className={style.item}><a href='/News'>News</a></div>
+            <div className={style.item}><a href='/Music'>Music</a></div>
+            <div className={style.item}><a href='/Settings'>Settings</a></div>
         </nav>
     );
 }
