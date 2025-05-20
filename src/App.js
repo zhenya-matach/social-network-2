152893661c71994644e014c22cee9ab9b17c8679
Yauheni Matach <zhenya.matach@gmail.com>
2025-05-20 11:36:18 +0300

    18. Page Dialogs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
-const App = () => {
+const App = (props) => {
     return (
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className='app-wrapper-content'>
+                {/*<Profile/>*/}
+                <Dialogs/>
+            </div>
         </div>
     );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={style.content}>
+        <div>
             <div><img
                 src="https://img.freepik.com/free-vector/low-poly-style-abstract-backdrop-modern-presentation_1017-53594.jpg?semt=ais_hybrid&w=740"
                 alt=""/></div>
