15c317175cf0419cd1abb64cac0cc69a75e43914
Yauheni Matach <zhenya.matach@gmail.com>
2025-05-28 16:21:14 +0300

    31. onClick, ref, VirtualDOM, refactor dialogs and myPosts components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,16 @@
 import style from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import React from 'react';
 
 
 const Dialogs = (props) => {
+
+    let newMessageElement = React.createRef();
+    let newMassage = () => {
+        let text =newMessageElement.current.value;
+        console.log(text);
+    }
 
     let dialogsElements = props.state.dialogsData.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messageElements = props.state.messagesData.map(m => <Message message={m.message}/>);
@@ -15,6 +22,10 @@ const Dialogs = (props) => {
             </div>
             <div className={style.messages}>
                 {messageElements}
+            </div>
+            <div className={style.inputBlock}>
+                <textarea ref={newMessageElement} placeholder='Enter your message' cols="30" rows="3"></textarea>
+                <div><button onClick={newMassage} className={style.button}>Add message</button></div>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,15 @@
 import style from './MyPosts.module.css';
 import Post from './Post/Post';
+import React from 'react';
 
 const MyPosts = (props) => {
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        console.log(text);
+    }
 
     let postsElement = props.state.postsData.map( p => <Post message={p.message} likeCount={p.likesCount} src={p.srcAvatar} /> )
     return (
@@ -9,10 +17,10 @@ const MyPosts = (props) => {
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea placeholder="Enter your message"></textarea>
+                    <textarea ref={newPostElement} placeholder="Enter your message"></textarea>
                 </div>
                 <div>
-                    <button>Add Post</button>
+                    <button className={style.button} onClick={addPost}>Add Post</button>
                 </div>
             </div>
             <div className={style.posts}>
