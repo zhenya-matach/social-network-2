8a0a13852e8fefe7db4a8d3a010ddae13f2d795f
Yauheni Matach <zhenya.matach@gmail.com>
2025-06-12 23:03:24 +0300

    39. Action creator, action type, refactoring

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,26 +1,29 @@
 import style from './MyPosts.module.css';
 import Post from './Post/Post';
 import React from 'react';
+import {addPostActionCreator, updatePostTextActionCreator} from '../../../redux/store';
 
 const MyPosts = (props) => {
 
     let newPostElement = React.createRef();
     let addPost = () => {
-        props.dispatch({type: 'ADD-POST'})
+        props.dispatch(addPostActionCreator())
     }
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.dispatch({type: 'UPDATE-POST-TEXT', newText: text});
+        props.dispatch(updatePostTextActionCreator(text));
     }
 
-    let postsElement = props.state.postsData.map( p => <Post  key={p.id} message={p.message} likeCount={p.likesCount} src={p.srcAvatar} /> )
+    let postsElement = props.state.postsData.map(p => <Post key={p.id} message={p.message} likeCount={p.likesCount}
+                                                            src={p.srcAvatar}/>)
 
     return (
         <div className={style.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea id={'postInput'} onChange={onPostChange} ref={newPostElement} value={props.state.newPostText} placeholder="Enter your message"></textarea>
+                    <textarea id={'postInput'} onChange={onPostChange} ref={newPostElement}
+                              value={props.state.newPostText} placeholder="Enter your message"></textarea>
                 </div>
                 <div>
                     <button className={style.button} onClick={addPost}>Add Post</button>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_POST_TEXT = 'UPDATE-POST-TEXT';
+
 let store = {
     _state: {
         profilePage: {
@@ -42,13 +45,37 @@ let store = {
             newDialogMessage: ''
         },
         sideBar: {
-            friendsData:[
-                {id: 1, name: 'Dimych', src: 'https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-man-avatar-image-for-profile-png-image_9197908.png'},
-                {id: 2, name: 'Andrey', src: 'https://icons.veryicon.com/png/o/miscellaneous/user-avatar/user-avatar-male-5.png'},
-                {id: 3, name: 'Svetlana', src: 'https://img.freepik.com/premium-vector/vector-image-female-photographer-with-simple-minimalist-flat-design_1213699-5312.jpg?semt=ais_hybrid'},
-                {id: 4, name: 'Yauheni', src: 'https://png.pngtree.com/png-vector/20230903/ourmid/pngtree-man-avatar-isolated-png-image_9935806.png'},
-                {id: 5, name: 'Julia', src: 'https://t4.ftcdn.net/jpg/02/79/66/93/360_F_279669366_Lk12QalYQKMczLEa4ySjhaLtx1M2u7e6.jpg'},
-                {id: 6, name: 'Valera', src: 'https://static.vecteezy.com/system/resources/previews/004/607/806/non_2x/man-face-emotive-icon-smiling-bearded-male-character-in-yellow-flat-illustration-isolated-on-white-happy-human-psychological-portrait-positive-emotions-user-avatar-for-app-web-design-vector.jpg'},
+            friendsData: [
+                {
+                    id: 1,
+                    name: 'Dimych',
+                    src: 'https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-man-avatar-image-for-profile-png-image_9197908.png'
+                },
+                {
+                    id: 2,
+                    name: 'Andrey',
+                    src: 'https://icons.veryicon.com/png/o/miscellaneous/user-avatar/user-avatar-male-5.png'
+                },
+                {
+                    id: 3,
+                    name: 'Svetlana',
+                    src: 'https://img.freepik.com/premium-vector/vector-image-female-photographer-with-simple-minimalist-flat-design_1213699-5312.jpg?semt=ais_hybrid'
+                },
+                {
+                    id: 4,
+                    name: 'Yauheni',
+                    src: 'https://png.pngtree.com/png-vector/20230903/ourmid/pngtree-man-avatar-isolated-png-image_9935806.png'
+                },
+                {
+                    id: 5,
+                    name: 'Julia',
+                    src: 'https://t4.ftcdn.net/jpg/02/79/66/93/360_F_279669366_Lk12QalYQKMczLEa4ySjhaLtx1M2u7e6.jpg'
+                },
+                {
+                    id: 6,
+                    name: 'Valera',
+                    src: 'https://static.vecteezy.com/system/resources/previews/004/607/806/non_2x/man-face-emotive-icon-smiling-bearded-male-character-in-yellow-flat-illustration-isolated-on-white-happy-human-psychological-portrait-positive-emotions-user-avatar-for-app-web-design-vector.jpg'
+                },
             ]
         }
     },
@@ -91,6 +118,10 @@ let store = {
         }
     }
 }
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+
+export const updatePostTextActionCreator = (text) => ({type: UPDATE_POST_TEXT, newText: text})
 
 window.store = store;
 
